93291bd9ae60df2f1794cdfb1d325c837e5ea0fd
kozikov <kozikov.ivan1@gmail.com>
2018-12-12 16:15:37 +0200

    final version

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   return gulp.src('sass/style.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
   });
   gulp.task('prefixer', () =>
   gulp.src('css/style.css')
@@ -16,9 +18,17 @@ gulp.task('sass', function () {
       }))
       .pipe(gulp.dest('public/css'))
 );
-
+//автоперезагрузка браузера
+gulp.task('serve', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./public"
+      }
+  });
+});
 //Задача, после запуска которой, gulp будет следить за изменениями файлов
 gulp.task('start', function () {
   gulp.watch('sass/style.sass', gulp.series('sass'));
   gulp.watch('css/style.css', gulp.series('prefixer'));
+  gulp.watch("public/css/*.css", gulp.series('serve')).on('change', browserSync.reload);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,31 @@
 $(window).scroll(function(){
     if ($(window).scrollTop() > 0) {
-        $('.row').addClass('scroll');
+        $('.header__row').addClass('header-scroll');
+        $('.header__logo').addClass('header__logo-scroll');
     }
     else {
-        $('.row').removeClass('scroll')
+        $('.header__row').removeClass('header-scroll');
+        $('.header__logo').removeClass('header__logo-scroll')
     }
 });
 
 
 
 $(function(){
-    $('.menu-btn').on('click',function(){
-        $('.menu-btn').toggleClass('cross');
-        $('.row.clearfix').toggleClass('open-nav');
-        $('.header-nav').toggleClass('grow');		
+    $('.header__btn').on('click',function(){
+        $('.header__btn').toggleClass('header__btn-cross');
+        $('.header__row').toggleClass('header__row-open');
+        $('.header__nav').toggleClass('header__nav-open');		
 	});
-    $('.joinUs h3').on('click',function(){
-        $('.joinUs ul').toggleClass('dropdown');	
+    $('.header__btn-joinUs h3').on('click',function(){
+        $('.header__list-joinUs').toggleClass('joinUs-dropdown');	
 	});
-	$('.language h3').on('click',function(){
-        $('.language h3').toggleClass('rotate');
-        $('.language ul').toggleClass('dropdown');	
+	$('.header__btn-language h3').on('click',function(){
+        $('.header__btn-language h3').toggleClass('header__btn-rotate');
+        $('.header__list-language').toggleClass('language-dropdown');	
     });
-    $('.language-footer h3').on('click',function(){
-        $('.language-footer ul').toggleClass('dropdown');
-        $('.language-footer h3').toggleClass('rotate');	
+    $('.footer__language h3').on('click',function(){
+        $('.footer__list-language').toggleClass('footer__list-dropdown');
+        $('.footer__language h3').toggleClass('footer__language-rotate');	
     });
 });
